de4fb033583695cb0cc88beaf21e61f987fea573
Fogang Rodriq <fogangrodriq@gmail.com>
2018-07-13 02:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ var a, b
   function add(a,b){
       return a+b;
   }
+  function subtract(a,b){
+      return a-b;
+  }
+  function multiply(a,b){
+      return a*b;
+  }
+  function divide(a,b){
+      return a/b;
+  }
+  function add(a,b){
+      return a+b;
+  }
   
